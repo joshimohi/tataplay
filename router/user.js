1e4debef532f32e4af64090d46cd68021316f1ae
mtbtv <mtbtv.official.2@gmail.com>
2024-10-04 10:27:45 +0000

    ss

--- router/user.js
+++ router/user.js
@@ -7,7 +7,6 @@ const {
   homeGet,
   ChannelGet,
   createUser,
-  playlistlGet,
   userCreate,
 } = require("../router_controller/controller");
 
@@ -16,6 +15,6 @@ router.post("/api/user/signup", userCreate);
 router.route("/api/user/:id").post(userLogin).patch(userUpdate);
 router.get("/", homeGet);
 router.get("/api/v1/channels", ChannelGet);
-router.get("/playlist/master.m3u", playlistlGet);
+router.get("/playlist/master.m3u", ChannelGet);
 
 module.exports = router;
