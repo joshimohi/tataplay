9f0e8e73ffc95eb1d7e93f775660a1365d3fb16a
mtbtv <mtbtv.official.2@gmail.com>
2025-05-22 01:23:09 +0530

    Update user.js

--- router/user.js
+++ router/user.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const {
-  userLogin,
-  alluserGet,
-  userUpdate,
+  // userLogin,
+  // alluserGet,
+  // userUpdate,
   homeGet,
   ChannelGet,
-  createUser,
-  userCreate,
+  // createUser,
+  // userCreate,
 } = require("../router_controller/controller");
 
-router.get("/api/user", alluserGet);
-router.post("/api/user/signup", userCreate);
-router.route("/api/user/:id").post(userLogin).patch(userUpdate);
+// // router.get("/api/user", alluserGet);
+// router.post("/api/user/signup", userCreate);
+// router.route("/api/user/:id").post(userLogin).patch(userUpdate);
 router.get("/", homeGet);
 router.get("/api/v1/channels", ChannelGet);
-router.get("/playlist/master.m3u", ChannelGet);
+// router.get("/playlist/master.m3u", ChannelGet);
 
 module.exports = router;
